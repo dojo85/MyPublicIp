46ee5a61612baa8ef16ef515166ce94e6feb6e6f
Niko D <nikodojan@live.de>
2021-12-01 14:51:25 +0100

    vue app created; endpoint route returns an object

---
+++ my-public-ip-vueapp/src/components/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <HelloWorld msg="this is my test page"/>
+</template>
+
+<script>
+
+import HelloWorld from './HelloWorld.vue'
+
+export default {
+    name: "App",
+    data (){
+
+    },
+    components: {
+        HelloWorld
+    },
+    methods:{
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
